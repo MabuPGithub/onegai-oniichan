fac0fc2fa936d38839ee3b6e9f0fb5cccb8ef109
MabuPGithub <mylespison@yahoo.com>
2021-08-28 12:38:47 +0800

    setup basic routes and pages

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+    def index
+
+    end
+
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to "/admins"
+      else
+        render :edit
+      end
+    end
+  
+    def account
+      
+    end
+
+    def nendoroids
+      
+    end
+    
+    private
+      def user_params
+        params.require(:user).permit(:name, :email, :phone_number, :balance, :address)
+      end
+end
