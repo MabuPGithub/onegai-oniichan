e5b4fc3e00b4bc0fbe472a71270fa8e9b069531f
MabuPGithub <mylespison@yahoo.com>
2021-09-07 11:39:39 +0800

    fixed order feature

---
+++ db/migrate/20210906125853_create_orders.rb
@@ -0,0 +1,14 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.integer :nendoroid_id
+      t.string :nendoroid_name
+      t.integer :quantity
+      t.decimal :total_price
+      t.boolean :deposit, default: false
+      t.string :status
+      t.string :reason
+      t.timestamps
+    end
+  end
+end
