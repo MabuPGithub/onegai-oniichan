fa09a7df8eea8218dd1aaca0b588d399c1eb2e13
MabuPGithub <mylespison@yahoo.com>
2021-09-10 18:31:11 +0800

    fixed bugs, added before actions and validations

--- app/models/nendoroid.rb
+++ app/models/nendoroid.rb
@@ -1,4 +1,17 @@
 class Nendoroid < ApplicationRecord
     has_one_attached :main_photo
     has_many_attached :photos
+
+    validates :name, presence: true
+    validates :nendoroid_number, presence: true
+    validates :series, presence: true
+    validates :simple_description, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+    validates :release_date, presence: true
+    validates :pre_order_price, presence: true
+    validates :pre_order_date, presence: true
+    validates :eta, presence: true
+    validates :min_deposit, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
 end
